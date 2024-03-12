4db686c2c12030b451ca5ba42578c8b6a1dc2328
Miguel Torrico <mickimaick18@gmail.com>
2024-03-12 16:06:00 -0400

    posible callback hell

--- ejemplos/callback.js
+++ ejemplos/callback.js
@@ -19,3 +19,32 @@ const persona = () => console.log("barbara") /*<-- esto es una aerofuntion*/
 persona ();
 
 () => console.log() /*<-- otra forma de hacer un aerofuntion */
+
+
+/* en caso de hacer un callback hell seria asi
+function uno(dos) {
+    console.log("uno")
+    setTimeout(dos, 2000)
+}
+
+function dos(tres) {
+    console.log("dos")
+    setTimeout(tres, 2000)
+}
+
+function tres(cuatro) {
+    console.log("tres")
+    setTimeout(cuatro, 2000)
+}
+
+function cuatro(cinco) {
+    console.log("cuatro")
+    setTimeout(cinco, 2000)
+}
+
+//calllback hell
+setTimeout(() => uno(
+    () => dos(
+        () => tres(
+            () => cuatro(
+                () => cinco(),)))), 3000)  */
