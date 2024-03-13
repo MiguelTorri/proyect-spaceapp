ed15a0b2cd5f877db27f295035471bd5dc983ebc
Miguel Torrico <mickimaick18@gmail.com>
2024-03-13 00:35:42 -0400

    aplicando async y await

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 
-const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15"
+const url = "https://api.nasa.gov/planetary/apod?api_key=bmUw4Wxrg4D5FsZAqpdjlI0AwSmEqAUmhIzBu4hb&count=15"
 
-function listaImagenes (){
+
+
+async function listaImagenes() {
+    try{
+        let fetchImagen = await fetch(url)
+        let datosImagenes = await fetchImagen.json()
+
+        const card = document.querySelector("[data-ul]")
+
+        datosImagenes.forEach( elemento => {
+           const contenido = `<li class="card">
+            <img class="card__image" src="${elemento.url}" alt="imagen">
+            <h3 class="card__title">${elemento.title}</h3>
+            </li>`
+            card.innerHTML = card.innerHTML + contenido
+        })
+
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+listaImagenes()
+
+
+
+
+
+
+/*function listaImagenes (){
     fetch(url)
     .then(response => response.json())
     .then (datosImagen => {
